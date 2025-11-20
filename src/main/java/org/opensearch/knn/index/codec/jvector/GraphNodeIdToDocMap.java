d27ce961b9cb3f0311a2c8c75e74f0940bd1b7f0
Samuel Herman <sherman8915@gmail.com>
2025-11-20 12:23:24 -0800

    remove redundant log and change to warn
    Signed-off-by: Samuel Herman <sherman8915@gmail.com>

--- src/main/java/org/opensearch/knn/index/codec/jvector/GraphNodeIdToDocMap.java
+++ src/main/java/org/opensearch/knn/index/codec/jvector/GraphNodeIdToDocMap.java
@@ -71,16 +71,9 @@ public class GraphNodeIdToDocMap {
         // We are going to assume that the number of ordinals is roughly the same as the number of documents in the segment, therefore,
         // the mapping will not be sparse.
         if (maxDocs < graphNodeIdsToDocIds.length) {
-            log.info(
+            log.warn(
                 "Max docs {} is less than the number of ordinals {}, this implies a lot of deleted documents. Or that some documents are missing vectors. Wasting a lot of memory",
                 maxDocs,
-                graphNodeIdsToDocIds.length
-            );
-        }
-        if (maxDocId > graphNodeIdsToDocIds.length) {
-            log.warn(
-                "Max doc id {} is greater than the number of ordinals {}, this implies a lot of deleted documents. Or that some documents are missing vectors. Wasting a lot of memory",
-                maxDocId,
                 graphNodeIdsToDocIds.length
             );
         }
