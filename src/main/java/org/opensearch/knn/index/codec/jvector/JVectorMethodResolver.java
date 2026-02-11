32a5686d866241ca2b7b04304afebb71dcb8c52f
Andriy Redko <drreta@gmail.com>
2026-02-11 13:05:12 -0500

    Some fixes related to derived fields
    Signed-off-by: Andriy Redko <drreta@gmail.com>

--- src/main/java/org/opensearch/knn/index/codec/jvector/JVectorMethodResolver.java
+++ src/main/java/org/opensearch/knn/index/codec/jvector/JVectorMethodResolver.java
@@ -5,10 +5,15 @@
 
 package org.opensearch.knn.index.codec.jvector;
 
+import java.util.HashMap;
+
+import org.opensearch.knn.common.KNNConstants;
 import org.opensearch.knn.index.SpaceType;
 import org.opensearch.knn.index.engine.AbstractMethodResolver;
+import org.opensearch.knn.index.engine.KNNEngine;
 import org.opensearch.knn.index.engine.KNNMethodConfigContext;
 import org.opensearch.knn.index.engine.KNNMethodContext;
+import org.opensearch.knn.index.engine.MethodComponentContext;
 import org.opensearch.knn.index.engine.ResolvedMethodContext;
 import org.opensearch.knn.index.mapper.CompressionLevel;
 
@@ -21,6 +26,22 @@ public class JVectorMethodResolver extends AbstractMethodResolver {
         boolean shouldRequireTraining,
         SpaceType spaceType
     ) {
-        return ResolvedMethodContext.builder().knnMethodContext(knnMethodContext).compressionLevel(CompressionLevel.x1).build();
+        return ResolvedMethodContext.builder()
+            .knnMethodContext(initResolvedKNNMethodContext(knnMethodContext, KNNEngine.JVECTOR, spaceType, KNNConstants.DISK_ANN))
+            .compressionLevel(CompressionLevel.x1)
+            .build();
+    }
+
+    protected KNNMethodContext initResolvedKNNMethodContext(
+        KNNMethodContext originalMethodContext,
+        KNNEngine knnEngine,
+        SpaceType spaceType,
+        String methodName
+    ) {
+        if (originalMethodContext == null) {
+            return new KNNMethodContext(knnEngine, spaceType, new MethodComponentContext(methodName, new HashMap<>()));
+        } else {
+            return new KNNMethodContext(originalMethodContext);
+        }
     }
 }
