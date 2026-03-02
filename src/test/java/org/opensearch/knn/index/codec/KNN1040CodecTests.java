1a33f400c9c7ee5476110abd46d74d592d4e4c4e
Anthony Leong <aj.leong623@gmail.com>
2026-03-02 13:55:40 -0800

    commit tests
    Signed-off-by: Anthony Leong <aj.leong623@gmail.com>

---
+++ src/test/java/org/opensearch/knn/index/codec/KNN1040CodecTests.java
@@ -0,0 +1,44 @@
+package org.opensearch.knn.index.codec;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.apache.lucene.codecs.Codec;
+import org.apache.lucene.codecs.perfield.PerFieldKnnVectorsFormat;
+import org.opensearch.index.mapper.MapperService;
+import org.opensearch.knn.index.codec.KNN10010Codec.KNN10010DerivedSourceStoredFieldsFormat;
+import org.opensearch.knn.index.codec.KNN1040Codec.KNN1040Codec;
+import org.opensearch.knn.index.codec.KNN80Codec.KNN80CompoundFormat;
+import org.opensearch.knn.index.codec.KNN80Codec.KNN80DocValuesFormat;
+import org.opensearch.knn.index.codec.KNN9120Codec.KNN9120PerFieldKnnVectorsFormat;
+
+import lombok.SneakyThrows;
+
+public class KNN1040CodecTests {
+
+    // Ensure that the codec is able to return the correct doc values format for codec
+    public void testCodecSetsDocValuesFormat() {
+        final Codec codec = new KNN1040Codec();
+        assertTrue(codec.docValuesFormat() instanceof KNN80DocValuesFormat);
+    }
+
+    // Ensure that the codec is able to return the correct compound format for codec
+    public void testCodecSetsCompoundFormat() {
+        final Codec codec = new KNN1040Codec();
+        assertTrue(codec.compoundFormat() instanceof KNN80CompoundFormat);
+    }
+
+    // Ensure that the codec is able to return the correct per field knn vectors format for codec
+    public void testCodecSetsCustomPerFieldKnnVectorsFormat() {
+        final Codec codec = new KNN1040Codec();
+        assertTrue(codec.knnVectorsFormat() instanceof KNN9120PerFieldKnnVectorsFormat);
+    }
+
+    // Ensure that the codec is able to return the correct stored fields format for codec
+    public void testCodecSetsStoredFieldsFormat() {
+        final Codec codec = new KNN1040Codec();
+        assertTrue(codec.storedFieldsFormat() instanceof KNN10010DerivedSourceStoredFieldsFormat);
+    }
+}
