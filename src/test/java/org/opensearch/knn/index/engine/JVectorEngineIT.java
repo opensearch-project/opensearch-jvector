2f497e0b107d123bd32e90bca03ca1ed6bb951ed
Andriy Redko <drreta@gmail.com>
2026-03-24 15:35:11 -0400

    Remove usage of the commons-lang 2.6
    Signed-off-by: Andriy Redko <drreta@gmail.com>

--- src/test/java/org/opensearch/knn/index/engine/JVectorEngineIT.java
+++ src/test/java/org/opensearch/knn/index/engine/JVectorEngineIT.java
@@ -10,7 +10,7 @@ import org.opensearch.client.*;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import lombok.SneakyThrows;
-import org.apache.commons.lang.math.RandomUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.junit.After;
@@ -287,7 +287,7 @@ public class JVectorEngineIT extends KNNRestTestCase {
         }
 
         final float[] searchVector = TEST_QUERY_VECTORS[0];
-        final int k = 1 + RandomUtils.nextInt(TEST_INDEX_VECTORS.length);
+        final int k = 1 + RandomUtils.secure().randomInt(0, TEST_INDEX_VECTORS.length);
 
         final List<float[]> knnResultsBeforeIndexClosure = queryResults(searchVector, k);
 
