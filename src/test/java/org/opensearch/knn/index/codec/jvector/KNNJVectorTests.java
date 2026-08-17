e15ecd2a712e3380c0fd137f4ace0e61981346bf
ergun-kezer <ergun.kezer@ibm.com>
2026-08-17 12:39:24 +0200

    fix: testJVectorKnnIndex_filter_maxInnerProduct test
    Signed-off-by: ergun-kezer <ergun.kezer@ibm.com>

--- src/test/java/org/opensearch/knn/index/codec/jvector/KNNJVectorTests.java
+++ src/test/java/org/opensearch/knn/index/codec/jvector/KNNJVectorTests.java
@@ -216,6 +216,7 @@ public class KNNJVectorTests extends LuceneTestCase {
         indexWriterConfig.setUseCompoundFile(false);
         indexWriterConfig.setCodec(getCodec(random().nextBoolean()));
         indexWriterConfig.setMergePolicy(new ForceMergesOnlyMergePolicy());
+        indexWriterConfig.setMaxBufferedDocs(totalNumberOfDocs);
         final Path indexPath = createTempDir();
         log.info("Index path: {}", indexPath);
         try (FSDirectory dir = FSDirectory.open(indexPath); IndexWriter w = new IndexWriter(dir, indexWriterConfig)) {
@@ -231,7 +232,8 @@ public class KNNJVectorTests extends LuceneTestCase {
             w.commit();
 
             try (IndexReader reader = DirectoryReader.open(w)) {
-                log.info("We should now have a single segment with 10 documents");
+                log.info("We should now have a single segment with {} documents", totalNumberOfDocs);
+
                 Assert.assertEquals(1, reader.getContext().leaves().size());
                 Assert.assertEquals(totalNumberOfDocs, reader.numDocs());
 
